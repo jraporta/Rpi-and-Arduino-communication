c18bbfcc7f0287c18fdf74a5cecca62df46d3ca0
Juan Ramon Porta <jraporta@gmail.com>
2019-11-02 10:12:27 +0100

    Modified to meet requirements

--- Rpi_and_arduino/Rpi_and_arduino.ino
+++ Rpi_and_arduino/Rpi_and_arduino.ino
@@ -1,28 +1,35 @@
 bool state=0;
+char message [3] = {'H','I','!'};
 
 void setup() {
   Serial.begin(9600);
   pinMode(LED_BUILTIN,OUTPUT);
   while(!Serial){ 
   }
-  Serial.println("Mkr1000 receiving");
+  Serial.println(message);
+  Serial.println("Mkr1000 recieving");
 }
 
 void loop() {
-  digitalWrite(LED_BUILTIN,state);
   if (Serial.available()){
-    if (state==0){
-      if(Serial.find("ON")){
-        state=1;
-        Serial.print ("I recieved... ");
-        Serial.println("ON");
-      }
-    }else if (state==1){
-      if(Serial.find("OFF")){
-        state=0;
-        Serial.print ("I recieved... ");
-        Serial.println("OFF");
-      }
-    }
+    recieveSerial();
+    checkMessage();
+  }
+  digitalWrite(LED_BUILTIN,state);
+}
+
+void recieveSerial(){
+  message[0]=message[1];
+  message[1]=message[2];
+  message[2]=Serial.read();
+}
+
+void checkMessage(){
+  if(message[0]=='O' && message[1]=='F' && message[2]=='F'){
+    Serial.println("LED OFF");
+    state=0;
+  }else if(message[1]=='O' && message[2]=='N'){
+    Serial.println("LED ON");
+    state=1;
   }
 }
